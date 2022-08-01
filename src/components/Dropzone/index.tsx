66c479aeb8003137e43eef660d23e3d0a9021147
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2022-08-01 12:16:21 -0700

    dropzone complete

---
+++ src/components/Dropzone/index.tsx
@@ -0,0 +1,90 @@
+/* eslint-disable prettier/prettier */
+import React, { useContext, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { IDropzone } from "src/@types";
+import { AuthContext } from "src/contexts/AuthContext";
+import api from "src/services/api";
+import { useSWRConfig } from "swr";
+
+import * as S from "./styles";
+
+const Dropzone: React.FC<IDropzone> = ({ imageName, setIsActive }) => {
+    const { user, setUser } = useContext(AuthContext);
+    const [isLoadingUpload, setIsLoadingUpload] = useState(false);
+    const { mutate } = useSWRConfig();
+
+    const onDropAccepted = async (file: File[]) => {
+        const inputFile = file[0];
+        const formData = new FormData();
+        formData.append("file", inputFile);
+        setIsLoadingUpload(true);
+        await api
+            .put(`/user/${user?.id}`, formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            })
+            .then(
+                (response: {
+                    data: { user: { avatar: any; id: any; name: any } };
+                }) => {
+                    setUser({
+                        avatar: response.data.user.avatar,
+                        id: response.data.user.id,
+                        name: response.data.user.name,
+                    });
+                    mutate(`/user/${user?.id}`);
+                },
+            );
+        setIsLoadingUpload(false);
+        setIsActive && setIsActive(false);
+    };
+    const { getRootProps, getInputProps, isDragReject, isDragActive } =
+        useDropzone({
+            maxFiles: 1,
+            maxSize: 5 * 1000 * 1000,
+            accept: {
+                "image/png": [".png"],
+                "image/jpg": [".jpg"],
+                "image/jpeg": [".jpeg"],
+            },
+            onDropAccepted,
+        });
+
+    return (
+        <>
+            <S.Container
+                {...getRootProps()}
+                isDragActive={isDragActive}
+                isDragReject={isDragReject}
+            >
+                <div className="dropzone__container">
+                    {isLoadingUpload ? (
+                        <Loading />
+                    ) : (
+                        <>
+                            <input
+                                {...getInputProps()}
+                                data-testid="input-file"
+                            />
+
+                            {isDragReject ? (
+                                <p className="error">
+                                    File not supported, drag and drop one file
+                                    jpg, png or jpeg. Maximum size: 5MB
+                                </p>
+                            ) : (
+                                <p>
+                                    Drag and drop one file here, or click here
+                                    to select file
+                                </p>
+                            )}
+                        </>
+                    )}
+                </div>
+            </S.Container>
+        </>
+    );
+};
+
+export default Dropzone;
---
+++ src/components/Dropzone/styles.ts
@@ -0,0 +1,53 @@
+import styled, { css } from "styled-components";
+
+import { IDropzoneStyle } from "../../@types";
+
+const dragActive = css`
+    border-color: ${({ theme }) => theme.button.primary};
+`;
+
+const dragReject = css`
+    border-color: red;
+`;
+
+export const Container = styled.div<IDropzoneStyle>`
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+    position: relative;
+    width: 50rem;
+    height: 40rem;
+    border: 0.1rem solid ${({ theme }) => theme.border.primary};
+    background-color: ${({ theme }) => theme.background.primary};
+    border-radius: 1rem;
+    .dropzone__container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 0.1rem dashed ${({ theme }) => theme.text.grey_100};
+        width: 95%;
+        height: 95%;
+        margin: auto;
+        border-radius: 1rem;
+        ${props => props.isDragActive && dragActive}
+        ${props => props.isDragReject && dragReject}
+    }
+    p {
+        font-size: 1.5rem;
+        font-weight: bold;
+        color: ${({ theme }) => theme.text.grey_100};
+    }
+    .error {
+        color: red;
+        text-align: center;
+    }
+    @media (max-width: 650px) {
+        width: 70vw;
+        height: 50vh;
+        p {
+            padding: 2rem;
+            text-align: center;
+            font-size: clamp(1.3rem, 2vw, 1.5rem);
+        }
+    }
+`;
